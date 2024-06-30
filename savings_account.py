f650f124fd11f1ae78d7d2d4adab71d7fbfcb913
ncmoliver <mrmarquesoliver@gmail.com>
2024-06-30 07:26:22 -0400

    badges

--- savings_account.py
+++ savings_account.py
@@ -18,23 +18,23 @@ def create_savings_account(balance, interest, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-    account = Account(balance, interest)
+    account = Account(balance, 0)
     
 
     # Calculate interest earned
      # ADD YOUR CODE HERE
-    interest_earned = account.balance * (account.interest/100) * (int(months)/12)
+    interest_earned = balance * (interest/100) * (int(months)/12)
     # Update the savings account balance by adding the 
     # ADD YOUR CODE HERE
-    balance = account.balance + interest_earned
+    updated_balance = balance + interest_earned
     # Pass the updated_balance to the set balance method 
     # using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
     
-    account.set_balance(balance)
+    account.set_balance(updated_balance)
     
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
     account.set_interest(interest_earned)
     # Return the updated balance and interest earned.
-    return  account.balance, account.interest
+    return  updated_balance, interest_earned
